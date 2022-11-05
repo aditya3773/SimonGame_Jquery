ffb4fc8db6cbf97d7b4a2f3dcbd0b8aae8536168
aditya3773 <aditya3773.codes@gmail.com>
2022-11-06 03:55:44 +0530

    Added clicks

--- game.js
+++ game.js
@@ -1,22 +1,31 @@
-const buttonColors = document.querySelectorAll(".btn");
+var buttonColors = ["red", "blue", "green", "yellow"];
+var userClickedPattern=[];
 const gamePattern=[];
 
+function animatePress(color){
+    $("#" + color).addClass("pressed");
+
+    setTimeout(function () {
+        $("#" + color).removeClass("pressed");
+      }, 100);
+}
+function playSound(color){
+    var audio = new Audio("sounds/" + color + ".mp3");
+    audio.play();
+}
+function clickHandler(){
+    userClickedPattern.push($(this).attr("id"));
+    playSound($(this).attr("id"));
+    animatePress($(this).attr("id"));
+    
+}
 function nextSequence(){
     return Math.floor(Math.random()*4);
 }
 
 randomChosenColor=buttonColors[nextSequence()];
 gamePattern.push(randomChosenColor);
+$("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+playSound(randomChosenColor);
 
-$(randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-if($(randomChosenColor).hasClass("green")){
-    var audio=new Audio('./sounds/green.mp3');
-    audio.play();
-}else{
-    var audio=new Audio('./sounds/green.mp3');
-    audio.play();
-}
-
-
-
-
+$(".btn").click(clickHandler);
